e774f306e405c6ba569c5167cdafd671743b1375
Gradt <carolinefell@yandex.ru>
2020-06-16 09:23:43 +0300

    Added title

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -4,7 +4,7 @@ import DateTimePretty from "../DateTimePretty/DateTimePretty";
 function Video(props) {
   return (
     <div className="video">
-      <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen />
+      <iframe src={props.url} title="title" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen />
       <DateTimePretty date={props.date} />
     </div>
   );
